0ccffd76f8f844a705475d0777dac8bd7c602bdc
visky-krisztina <127683091+visky-krisztina@users.noreply.github.com>
2024-06-18 00:37:23 +0300

    changed vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -14,4 +14,15 @@ export default defineConfig({
 			},
 		},
 	},
+	version: 2,
+	builds: [
+		{
+			src: "package.json",
+			use: "@vercel/static-build",
+			config: {
+				distDir: "build",
+			},
+		},
+	],
+	routes: [{ src: "/(.*)", dest: "/index.html" }],
 });
